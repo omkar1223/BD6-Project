4f6ed1005f54ff40db7a1b874379336562790333
New User <38864306-jefabo9067@users.noreply.replit.com>
2025-01-13 19:16:57 +0000

    added new files and code

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-import express from 'express';
-
+let express = require("express");
+let { getAllStocks } = require("./controllers");
 const app = express();
+app.use(express.json());
+app.use(express.static("static"));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/stocks", async (req, res) => {
+  const result = await getAllStocks();
+  res.json({ stocks: result });
 });
 
 app.listen(3000, () => {
-  console.log('Express server initialized');
+  console.log("Express server initialized");
 });
